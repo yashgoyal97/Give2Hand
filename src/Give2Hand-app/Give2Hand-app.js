fc8cb21be2a0b5a183c2d89d41e22aae34e4021c
yash <yg311997@gmail.com>
2020-02-14 11:11:25 +0530

    changes in polymer json

--- src/Give2Hand-app/Give2Hand-app.js
+++ src/Give2Hand-app/Give2Hand-app.js
@@ -1,4 +1,11 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
+import '@polymer/app-route/app-location.js';
+import '@polymer/app-route/app-route.js';
+import '@polymer/iron-pages/iron-pages.js';
+
+setPassiveTouchGestures(true);
+setRootPath(MyAppGlobals.rootPath);
 
 /**
  * @customElement
@@ -7,21 +14,62 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 class Give2HandApp extends PolymerElement {
   static get template() {
     return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
+    <app-location route="{{route}}" url-space-regex="^[[rootPath]]" use-hash-as-path></app-location>
+    <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}"></app-route>     
+    <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+      <home-page name="home"></home-page>
+      <view404-page name='view404'></view404-page>
+    </iron-pages>
     `;
   }
+
   static get properties() {
     return {
-      prop1: {
+      page: {
         type: String,
-        value: 'Give2Hand-app'
-      }
+        reflectToAttribute: true,
+        observer: '_pageChanged'
+      },
+      routeData: Object,
+      subroute: Object
     };
+  }
+
+  static get observers() {
+    return [
+      '_routePageChanged(routeData.page)'
+    ];
+  }
+
+   /**
+   * 
+   * @param {String} page 
+   */
+  _routePageChanged(page) {
+    console.log(page)
+    if (!page) {
+      this.page = 'home';
+    } else if (['home'].indexOf(page) !== -1) {
+      this.page = page;
+    } else {
+      this.page = 'view404';
+    }
+  }
+
+  /**
+   * 
+   * @param {String} page 
+   */
+  _pageChanged(page) {
+    console.log(page)
+    switch (page) {
+      case 'home':
+        import('./home-page.js');
+        break;
+      case 'view404':
+        import('./view404-page.js');
+        break;
+    }
   }
 }
 
